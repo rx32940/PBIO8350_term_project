dc3c854f220f3938fc7c8c3d502f0b5da370c211
rx32940 <rx32940@uga.edu>
2020-04-23 14:41:15 -0400

    run roary with outgroup -build pangenome

--- run_iqtree.sh
+++ run_iqtree.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-#PBS -q highmem_q                                                            
+#PBS -q batch                                                            
 #PBS -N iqtree_interrogans                                    
-#PBS -l nodes=1:ppn=24 -l mem=300gb                                        
+#PBS -l nodes=1:ppn=24
+#PBS -l mem=10gb                                        
 #PBS -l walltime=300:00:00                                                
 #PBS -M rx32940@uga.edu                                                  
 #PBS -m abe                                                              
@@ -11,4 +12,4 @@
 
 module load IQ-TREE/1.6.5-omp
 
-iqtree -nt AUTO -m MFP -pre /home/rx32940/PBIO8350_term_project/interrogans_host_unrooted -s /home/rx32940/PBIO8350_term_project/roary_host/core_gene_alignment.aln
+iqtree -nt AUTO -m MFP -b 100 -pre /home/rx32940/PBIO8350_term_project/interrogans_host_unrooted -s /home/rx32940/PBIO8350_term_project/roary_host/core_gene_alignment.aln
--- run_roary.sh
+++ run_roary.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-#PBS -q highmem_q                                                            
+#PBS -q batch                                                            
 #PBS -N roary_host                                       
-#PBS -l nodes=1:ppn=12 -l mem=100gb                                        
+#PBS -l nodes=1:ppn=12 -l mem=80gb                                        
 #PBS -l walltime=300:00:00                                                
 #PBS -M rx32940@uga.edu                                                  
 #PBS -m abe                                                              
-#PBS -o /scratch/rx32940                        
-#PBS -e /scratch/rx32940                        
+#PBS -o /home/rx32940/PBIO8350_term_project                        
+#PBS -e /home/rx32940/PBIO8350_term_project     
 #PBS -j oe
 
 
 module load Roary/3.12.0
 
-# roary -e -n -p 12 /home/rx32940/PBIO8350_term_project/host_gff/*.gff -v -f /home/rx32940/PBIO8350_term_project/
+# -n mafft, otherwise prank codon aware alignment
+roary -e -p 12 /home/rx32940/PBIO8350_term_project/host_gff/*.gff -v -f /home/rx32940/PBIO8350_term_project/
