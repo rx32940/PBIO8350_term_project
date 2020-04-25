e7802dc8b0381c930b27477963b230844282b52f
rx32940 <rx32940@uga.edu>
2020-04-25 09:26:15 -0400

    core with outgroup iqtree

--- run_iqtree.sh
+++ run_iqtree.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #PBS -q batch                                                            
-#PBS -N iqtree_interrogans                                    
+#PBS -N iqtree_outgroup_core                                    
 #PBS -l nodes=1:ppn=24
 #PBS -l mem=10gb                                        
 #PBS -l walltime=300:00:00                                                
 #PBS -M rx32940@uga.edu                                                  
 #PBS -m abe                                                              
-#PBS -o /scratch/rx32940                        
-#PBS -e /scratch/rx32940                        
+#PBS -o /home/rx32940/PBIO8350_term_project
+#PBS -e /home/rx32940/PBIO8350_term_project     
 #PBS -j oe
 
 module load IQ-TREE/1.6.5-omp
 
-iqtree -nt AUTO -m MFP -b 100 -pre /home/rx32940/PBIO8350_term_project/interrogans_host_unrooted -s /home/rx32940/PBIO8350_term_project/roary_host/core_gene_alignment.aln
+iqtree -nt AUTO -m MFP -b 100 -o outgroup_biflexa -pre /home/rx32940/PBIO8350_term_project/outgroup_roary/iqtree/outgroup_core_interrogans -s /home/rx32940/PBIO8350_term_project/outgroup_roary/core_gene_alignment.aln
